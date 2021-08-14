03d85b83e4807936cdf3db5df9ef743e8404d21a
lixm <ximeng.li.87@hotmail.com>
2021-08-14 16:33:22 +0800

    some further cleanups

--- src/fun/cfm.v
+++ src/fun/cfm.v
@@ -88,36 +88,6 @@ Proof.
     do 2!(move->)=>//. 
 Qed. 
   
-(* Lemma zlcfm_infer_preserve: *)
-(*   forall lcf cf param,  *)
-(*     is_zlcfm lcf -> *)
-(*     infer param (mg_spec param) (lcf, cf) -> *)
-(*     cf = lcf.  *)
-(* Proof. *)
-(*   elim; try (move=> *; inv_zlcfm; fail). *)
-(*   - *)
-(*     move=> cf prm H_zlcfm H_inf. inv H_inf. *)
-(*     inv H1; subst. auto. *)
-(*   - *)
-(*     move=> e IH1 e0 IH2 cf prm H_zlcfm H_inf. *)
-(*     inversion H_zlcfm; subst. *)
-(*     case En: e; rewrite En in H0; simpl in H0; inv H0.  *)
-(*     inversion H_inf; subst. *)
-(*     rename H3 into H_rule; inversion H_rule; subst. *)
-(*     ith_prem_exe_0 H4 h' mg_spec. clear h. *)
-(*     ith_prem_1 H4 H'. *)
-(*     have: ~(exists P, mg_spec prm e0 P) by apply zlcfm_not_in_spec; auto. *)
-(*     inversion H' as [H_ne | H_e]; clear H'. *)
-(*     + *)
-(*       move=> H_ne'. inv H_ne. *)
-(*       move: IH2=>/(_ cf' prm H0)=>IH2. apply IH2 in H2. rewrite H2=>//. *)
-(*     + *)
-(*       move=> H_ne'. inv H_e. inv H. *)
-(*       have H_ex: exists P, mg_spec prm e0 P.  *)
-(*       { exists x; auto. } *)
-(*       apply H_ne' in H_ex. inv H_ex. *)
-(* Qed. *)
-
 Lemma zlcfm_infer_nil {PARAM: Type}:
   forall (prm: PARAM) phi lcf, 
     is_zlcfm lcf ->
@@ -131,19 +101,6 @@ Proof.
     inv H_inf; subst.
     inv H1; subst.
 Qed.     
-
-(* Lemma zlcfm_infer_nil: *)
-(*   forall lcf param, *)
-(*     is_zlcfm lcf -> *)
-(*     infer param (mg_spec param) (lcf, NilE) -> *)
-(*     lcf = NilE. *)
-(* Proof. *)
-(*   elim; try (move=> *; inv_zlcfm; fail). *)
-(*   - try done. *)
-(*   - move=> e IH1 e0 IH2 prm H_zlcfm H_inf. *)
-(*     inv H_inf; subst. *)
-(*     inv H1; subst. *)
-(* Qed. *)
 
 Ltac get_branch hyp_or hyp_nex :=
         let h1 := fresh "h" in (
@@ -197,39 +154,6 @@ Proof.
     exists z, e2. apply: conj=>//.
 Qed.     
 
-(* Lemma zlcfm_infer_pend0:  *)
-(*   forall lcf param cf cf',  *)
-(*     is_zlcfm lcf ->  *)
-(*     infer param (mg_spec param) (lcf, PPendE cf cf') -> *)
-(*     exists z lcf'', *)
-(*       lcf = PPendE (NE z) lcf'' /\ *)
-(*       cf = (NE z) /\  *)
-(*       cf' = lcf'' /\ *)
-(*       is_zlcfm lcf''. *)
-(* Proof. *)
-(*   elim; try (move=> *; inv_zlcfm; fail). *)
-(*   - *)
-(*     move=> param cf cf' H_zlcfm H_inf. *)
-(*     inv H_inf. inv H1. *)
-(*   - *)
-(*     move=> e IH1 e0 IH2 prm cf cf' H_zlcfm H_inf. *)
-(*     inversion H_zlcfm; subst. *)
-(*     case En: e; rewrite En in H0; simpl in H0; inv H0. *)
-(*     inversion H_inf; subst. *)
-(*     inversion H3; subst. *)
-(*     eexists. exists e0. *)
-(*     apply: conj. eauto. apply: conj.  *)
-(*     ith_prem_exe_0 H4 h' mg_spec; auto. *)
-(*     ith_prem_1 H4 h'. *)
-(*     have H_nex: ~(exists P, mg_spec prm e0 P) *)
-(*       by apply zlcfm_not_in_spec; auto. *)
-(*     have H_inf0: infer prm (mg_spec prm) (e0, cf') *)
-(*       by get_branch h' H_nex. *)
-(*     apply: conj. *)
-(*     apply zlcfm_infer_preserve with (param:=prm); auto. *)
-(*     auto. *)
-(* Qed. *)
-
 Lemma zlcfm_infer_pend {PARAM: Type}: 
   forall (prm: PARAM) phi lcf cf cf',
     cfm_not_in_spec prm phi ->
@@ -251,24 +175,4 @@ Proof.
   inv H_ex0. inv H. inv H0. inv H2. inv H4.
   exists x. apply: conj. congruence. apply: conj=>//. congruence.
 Qed.   
-
-(* Lemma zlcfm_infer_pend:  *)
-(*   forall lcf param cf cf',  *)
-(*     is_zlcfm lcf ->  *)
-(*     infer param (mg_spec param) (lcf, PPendE cf cf') -> *)
-(*     exists z, *)
-(*       lcf = PPendE (NE z) cf' /\ *)
-(*       cf = (NE z) /\  *)
-(*       is_zlcfm cf'. *)
-(* Proof. *)
-(*   move=> lcf prm cf cf' H_zlcfm H_inf. *)
-(*   have H_ex0: *)
-(*     exists z lcf'', *)
-(*       lcf = PPendE (NE z) lcf'' /\ *)
-(*       cf = (NE z) /\  *)
-(*       cf' = lcf'' /\ *)
-(*       is_zlcfm lcf'' by apply zlcfm_infer_pend0 with (param:=prm); auto. *)
-(*   inv H_ex0. inv H. inv H0. inv H2. inv H4. *)
-(*   exists x. apply: conj. congruence. apply: conj=>//. congruence. *)
-(* Qed. *)
 
